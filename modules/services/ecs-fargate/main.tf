c0fa4f1d4751439c228bf844c6557f5ff7c50e4c
Toel Teemaa <toel.teemaa@cgi.com>
2023-04-25 11:52:05 +0300

    Simplified ECS configuration

--- modules/services/ecs-fargate/main.tf
+++ modules/services/ecs-fargate/main.tf
@@ -1,23 +1,43 @@
-module "ecs" {
-  source = "terraform-aws-modules/ecs/aws"
+provider "aws" {
+  region = local.region
+}
 
-  cluster_name = "ecs-integrated"
+locals {
+  region = "us-east-2"
+  name   = "ecs-ex-${replace(basename(path.cwd), "_", "-")}"
+/* 
+  tags = {
+    Name       = local.name
+    Example    = local.name
+    Repository = "https://github.com/terraform-aws-modules/terraform-aws-ecs"
+     */
+}
+
+module "ecs" {
+  source = "../.."
+
+  cluster_name = local.name
 
   cluster_configuration = {
-    /*
+/*    
     execute_command_configuration = {
       logging = "OVERRIDE"
       log_configuration = {
-        cloud_watch_log_group_name = "/aws/ecs/aws-ec2"
+        # You can set a simple string and ECS will create the CloudWatch log group for you
+        # or you can create the resource yourself as shown here to better manage retetion, tagging, etc.
+        # Embedding it into the module is not trivial and therefore it is externalized
+        cloud_watch_log_group_name = aws_cloudwatch_log_group.this.name
       }
     }
     */
   }
 
+  # Capacity provider
   fargate_capacity_providers = {
     FARGATE = {
       default_capacity_provider_strategy = {
         weight = 50
+        base   = 20
       }
     }
     FARGATE_SPOT = {
@@ -27,106 +47,19 @@ module "ecs" {
     }
   }
 
-  services = {
-    ecsdemo-frontend = {
-      cpu    = 1024
-      memory = 4096
-
-      # Container definition(s)
-      container_definitions = {
-
-        fluent-bit = {
-          cpu       = 512
-          memory    = 1024
-          essential = true
-          image     = "906394416424.dkr.ecr.us-west-2.amazonaws.com/aws-for-fluent-bit:stable"
-          firelens_configuration = {
-            type = "fluentbit"
-          }
-          memory_reservation = 50
-        }
-
-        ecs-sample = {
-          cpu       = 512
-          memory    = 1024
-          essential = true
-          image     = "public.ecr.aws/aws-containers/ecsdemo-frontend:776fd50"
-          port_mappings = [
-            {
-              name          = "ecs-sample"
-              containerPort = 80
-              protocol      = "tcp"
-            }
-          ]
-
-          # Example image used requires access to write to root filesystem
-          readonly_root_filesystem = false
-
-          dependencies = [{
-            containerName = "fluent-bit"
-            condition     = "START"
-          }]
-
-          enable_cloudwatch_logging = false
-          log_configuration = {
-            logDriver = "awsfirelens"
-            options = {
-              Name                    = "firehose"
-              region                  = "eu-west-1"
-              delivery_stream         = "my-stream"
-              log-driver-buffer-limit = "2097152"
-            }
-          }
-          memory_reservation = 100
-        }
-      }
-
-      service_connect_configuration = {
-        namespace = "example"
-        service = {
-          client_alias = {
-            port     = 80
-            dns_name = "ecs-sample"
-          }
-          port_name      = "ecs-sample"
-          discovery_name = "ecs-sample"
-        }
-      }
-
-/*       
-      load_balancer = {
-        service = {
-          target_group_arn = "arn:aws:elasticloadbalancing:eu-west-1:1234567890:targetgroup/bluegreentarget1/209a844cd01825a4"
-          container_name   = "ecs-sample"
-          container_port   = 80
-        }
-      } 
- */
-/*
-      subnet_ids = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
-      security_group_rules = {
-        alb_ingress_3000 = {
-          type                     = "ingress"
-          from_port                = 80
-          to_port                  = 80
-          protocol                 = "tcp"
-          description              = "Service port"
-          source_security_group_id = "sg-12345678"
-        }
-        egress_all = {
-          type        = "egress"
-          from_port   = 0
-          to_port     = 0
-          protocol    = "-1"
-          cidr_blocks = ["0.0.0.0/0"]
-        }
-      }
-    }
-  }
-/* 
-  tags = {
-    Environment = "Development"
-    Project     = "Example"
-  }
-   */
+  # tags = local.tags
 }
+/*
+module "ecs_disabled" {
+  source = "../.."
+
+  create = false
+}
+
+resource "aws_cloudwatch_log_group" "this" {
+  name              = "/aws/ecs/${local.name}"
+  retention_in_days = 7
+
+  tags = local.tags
+}
+*/
