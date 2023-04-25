c3d07d3a9dfbe12ad217ad0690952623bb78aa61
Toel Teemaa <toel.teemaa@cgi.com>
2023-04-25 12:16:30 +0300

    Simplified ECS configuration even more.

--- modules/services/ecs-fargate/main.tf
+++ modules/services/ecs-fargate/main.tf
@@ -14,40 +14,18 @@ locals {
 }
 
 module "ecs" {
-  source = "../.."
+  source  = "terraform-aws-modules/ecs/aws"
+  version = "5.0.0"
 
   cluster_name = local.name
 
-  cluster_configuration = {
-/*    
-    execute_command_configuration = {
-      logging = "OVERRIDE"
-      log_configuration = {
-        # You can set a simple string and ECS will create the CloudWatch log group for you
-        # or you can create the resource yourself as shown here to better manage retetion, tagging, etc.
-        # Embedding it into the module is not trivial and therefore it is externalized
-        cloud_watch_log_group_name = aws_cloudwatch_log_group.this.name
-      }
-    }
-    */
-  }
-
   # Capacity provider
   fargate_capacity_providers = {
-    FARGATE = {
-      default_capacity_provider_strategy = {
-        weight = 50
-        base   = 20
-      }
-    }
-    FARGATE_SPOT = {
-      default_capacity_provider_strategy = {
-        weight = 50
-      }
-    }
-  }
+    FARGATE = {}
+    FARGATE_SPOT = {}
 
   # tags = local.tags
+  }
 }
 /*
 module "ecs_disabled" {
