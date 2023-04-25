79e60a6616f7bf883e64e4dfeb33e95fb13af26a
Toel Teemaa <toel.teemaa@techyon.io>
2023-04-25 09:54:58 +0300

    Added ECS Fargate configurations

--- main.tf
+++ main.tf
@@ -2,8 +2,13 @@ module "networking" {
   source = "./modules/networking"
 }
 
+module "ecs-fargate" {
+  source = "./modules/services/ecs-fargate"
+  vpc_id = module.networking.vpc_id
+}
+
 /* module "rds" {
-  source = "./modules/rds"
+  source = "./modules/services/rds"
   vpc_id = module.networking.vpc_id
   private_subnets = module.networking.private_subnets
 } */
---
+++ modules/services/ecs-fargate/variables.tf
@@ -0,0 +1,5 @@
+variable "vpc_id" {
+    description = "VPC ID to tie your cluster to"
+    type = string
+    default = ""
+}
