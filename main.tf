3e0c94c20d97d4cd389db02f39f6b45245d8c4d9
Toel Teemaa <toel.teemaa@techyon.io>
2023-04-28 08:51:11 +0300

    Starting with manual VPC

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-module "networking" {
+/* module "networking" {
   source = "./modules/networking"
-} 
+}  */
  
-module "ecs-fargate" {
+/* module "ecs-fargate" {
   source = "./modules/services/ecs-fargate"
-}
+} */
 
 /* module "s3" {
   source = "./global/s3"
 } */
 
-module "ecr" {
+/* module "ecr" {
   source = "./modules/services/ecr"
-}
+} */
 
 /* module "rds-aurora" {
   source = "./modules/services/rds-aurora"
